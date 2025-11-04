fc994edbaefcbb3de32f6b4ecd52782fd19191f6
Kauê Reis Rodrigues <kaue.rodrigues@redasset.com.br>
2025-11-04 16:36:34 -0300

    feat: Estrutura Inical completa com projetos .NET e Web

---
+++ frontend-desktop/HelpDesk.Desktop/Form1.cs
@@ -0,0 +1,9 @@
+namespace HelpDesk.Desktop;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ frontend-mobile/HelpDesk.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HelpDesk.Mobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
